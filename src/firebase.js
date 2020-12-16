a598e368165f344a7f1065e19471834d339e07c2
Mateusz <youngbastardps4@gmail.com>
2020-12-16 17:44:49 +0100

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVJJAWxJQ4egoUZU3enx4mgbIPsfMh7qE",
+    authDomain: "tik-tok-clone-adfb9.firebaseapp.com",
+    projectId: "tik-tok-clone-adfb9",
+    storageBucket: "tik-tok-clone-adfb9.appspot.com",
+    messagingSenderId: "630892589869",
+    appId: "1:630892589869:web:265d8bfcaa0ea091eb59a4",
+    measurementId: "G-KCW45PE864"
+};
